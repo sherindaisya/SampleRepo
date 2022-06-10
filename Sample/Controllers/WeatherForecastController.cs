74b16aa1137098b1672fa71066d4fcdf5e7a4ef7
Sherin R <sherindaisya@gmail.com>
2022-06-10 16:30:07 +0530

    Changes in weather forecast

--- Sample/Controllers/WeatherForecastController.cs
+++ Sample/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace Sample.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Chill"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
